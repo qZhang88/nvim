05f8162bed9109af87c8fec5f9b0b0979d9c8f74
Qiao.Zhang <qzhang8806@gmail.com>
2025-11-18 15:34:00 +0800

    fix: sign_define() is deprecated

--- lua/usr/lsp/handlers.lua
+++ lua/usr/lsp/handlers.lua
@@ -11,16 +11,17 @@ M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 
 M.setup = function()
   local signs = {
-
-    { name = "DiagnosticSignError", text = "" },
-    { name = "DiagnosticSignWarn",  text = "" },
-    { name = "DiagnosticSignHint",  text = "" },
-    { name = "DiagnosticSignInfo",  text = "" },
+    text = {
+      { name = "DiagnosticSignError", text = "" },
+      { name = "DiagnosticSignWarn",  text = "" },
+      { name = "DiagnosticSignHint",  text = "" },
+      { name = "DiagnosticSignInfo",  text = "" },
+    }
   }
 
-  for _, sign in ipairs(signs) do
-    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-  end
+  -- for _, sign in ipairs(signs) do
+  --   vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  -- end
 
   local config = {
     virtual_text = true, -- disable virtual text
